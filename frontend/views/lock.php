7a4bed13db75ba3f88fe7fe4372a3d3eeb185f9a
Mahdi Abedi <abedi@jeyserver.com>
2017-07-25 13:36:46 +0430

    Fix #27

--- frontend/views/lock.php
+++ frontend/views/lock.php
@@ -1,19 +1,14 @@
 <?php
 namespace themes\clipone\views;
 use \packages\base\translator;
-use \packages\base\frontend\theme;
 use \packages\userpanel\views\lock as lockView;
+use \themes\clipone\viewTrait;
 use \themes\clipone\views\formTrait;
 class lock extends lockView{
-	use formTrait;
+	use viewTrait, formTrait;
 	protected $user;
 	function __beforeLoad(){
 		$this->setTitle(translator::trans('unlock'));
 		$this->user = $this->getUser();
-		$this->addAssets();
-	}
-	private function addAssets(){
-		$this->addJSFile(theme::url('assets/plugins/jquery-validation/dist/jquery.validate.min.js'));
-		$this->addJSFile(theme::url('assets/js/pages/lock.js'));
 	}
 }
