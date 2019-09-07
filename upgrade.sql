86117d1739f4d8aed6df55a9b31a810790513d27
Mahdi Abedi <abedi@jeyserver.com>
2019-09-07 14:24:49 +0430

    update upgrade sql file

--- upgrade.sql
+++ upgrade.sql
@@ -3,3 +3,8 @@
 ---	Commit:	1397fa971306a3155582a79652ce5c3ab03daf4b
 ---
 ALTER TABLE `userpanel_users` CHANGE `credit` `credit` INT(11) NOT NULL DEFAULT '0';
+
+--
+-- Commit: e0384b52ee070f29103aa85610c3697e6e9af005
+--
+ALTER TABLE `userpanel_users` ADD `registered_at` INT UNSIGNED NOT NULL AFTER `avatar`; 
