bb1bbfe75b00c1ead4ecee03309d89ff3e259f10
HosseinHosni <hosseinhosni7@gmail.com>
2019-09-11 13:31:07 +0430

    Fix #34

--- frontend/header.php
+++ frontend/header.php
@@ -1,12 +1,11 @@
 <?php
-use \packages\base;
-use \packages\base\translator;
-use \packages\base\frontend\theme;
-use \packages\userpanel;
-use \packages\userpanel\authentication;
-use \themes\clipone\navigation;
-use \themes\clipone\breadcrumb;
-?><!DOCTYPE html>
+use packages\base;
+use packages\base\{frontend\Theme, Translator};
+use packages\userpanel;
+use packages\userpanel\Authentication;
+use themes\clipone\{breadcrumb, Navigation, ViewTrait};
+?>
+<!DOCTYPE html>
 <!--[if IE 8]><html class="ie8 no-js" lang="en"><![endif]-->
 <!--[if IE 9]><html class="ie9 no-js" lang="en"><![endif]-->
 <!--[if !IE]><!-->
@@ -17,6 +16,11 @@ use \themes\clipone\breadcrumb;
 		<meta charset="utf-8" />
 		<!--[if IE]><meta http-equiv='X-UA-Compatible' content="IE=edge,IE=9,IE=8,chrome=1" /><![endif]-->
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
+		<?php
+			if ($this->getFavicon()) {
+				echo '<link rel="icon" href="' . $this->getFavicon() . '" type="image/x-icon" />';
+			}
+		?>
 		<meta name="apple-mobile-web-app-capable" content="yes">
 		<meta name="apple-mobile-web-app-status-bar-style" content="black">
 		<?php
