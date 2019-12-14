6b3bd4ce0c29f4681b02500cd93837bb7495087a
Hossein Hosni <hosni.hossein@gmail.com>
2019-12-14 16:23:57 +0330

    Fix #53

--- controllers/resetpwd.php
+++ controllers/resetpwd.php
@@ -1,50 +1,39 @@
 <?php
 namespace packages\userpanel\controllers;
-use \packages\base\http;
-use \packages\base\cache;
-use \packages\base\options;
-use \packages\base\NotFound;
-use \packages\base\translator;
-use \packages\base\view\error;
-use \packages\base\inputValidation;
-use \packages\base\views\FormError;
-use \packages\userpanel;
-use \packages\userpanel\date;
-use \packages\userpanel\controller;
-use \packages\userpanel\view;
-use \packages\userpanel\log;
-use \packages\userpanel\user;
-use \packages\sms\api as smsAPI;
-use \packages\email\api as emailAPI;
-use \packages\userpanel\authentication;
-use \packages\userpanel\resetpwd\token;
-class resetpwd  extends controller{
+
+use packages\base\{Cache, view\Error, Exception, views\FormError, http, InputValidation, NotFound, Options};
+use packages\notifications\events\Channels;
+use packages\userpanel;
+use packages\userpanel\{Authentication, Controller, Date, events, Log, resetpwd\Token, User, View, views};
+
+class resetpwd  extends Controller {
+
 	protected $authentication = false;
-	public function view(){
-		$view = view::byName("\\packages\\userpanel\\views\\resetpwd");
-		$inputsRules = [
-			'username' => [
-				'type' => ['email', 'cellphone'],
-				'optional' => true
-			],
-			'method' => [
-				'type' => 'string',
-				'values' => ['sms', 'email'],
-				'optional' => true
-			]
-		];
-		try{
+
+	public function view() {
+		$view = View::byName(views\resetpwd::class);
+		$view->setData($this->getChannelsNames(), "channelsnames");
+		try {
 			$this->haveChance();
-			$inputs = $this->checkinputs($inputsRules);
-			if(isset($inputs['username'])){
-				$view->setDataForm($inputs['username'], 'username');
+			$inputs = $this->checkinputs(array(
+				'username' => array(
+					'type' => array('email', 'cellphone'),
+					'optional' => true
+				),
+				'method' => array(
+					'type' => 'string',
+					'values' => $this->getChannelsNames(),
+					'optional' => true
+				),
+			));
+			foreach (array("username", "method") as $item) {
+				if (isset($inputs[$item])){
+					$view->setDataForm($inputs[$item], $item);
+				}
 			}
-			if(isset($inputs['method'])){
-				$view->setDataForm($inputs['method'], 'method');
-			}
-		}catch(inputValidation $error){
+		} catch (InputValidation $error) {
 			$view->setFormError(FormError::fromException($error));
-		}catch(losingChance $error){
+		} catch (losingChance $error) {
 			$error = $this->losingChanceError();
 			$view->addError($error);
 		}
@@ -52,72 +41,41 @@ class resetpwd  extends controller{
 		$this->response->setView($view);
 		return $this->response;
 	}
-	private function haveChance(){
-		$cache = new cache();
-		$times = options::get('userpanel.resetpwd.mis-chance.count');
-		if($cache->get("packages.userpanel.resetpwd.bruteforce.".http::$client['ip']) > $times){
-			throw new losingChance();
-		}
-	}
-	private function losingChanceError(){
-		$period = options::get('userpanel.resetpwd.mis-chance.period');
-		$times = options::get('userpanel.resetpwd.mis-chance.count');
-		$error = new error();
-		$error->setCode('userpanel.resetpwd.losingChance');
-		$error->setMessage(translator::trans('error.userpanel.resetpwd.losingChance', [
-			'times' => $times,
-			'expire_at' => date::relativeTime(date::time() + $period)
-		]));
-		return $error;
-	}
-	private function loseOneChance(){
-		$cache = new cache();
-		$period = options::get('userpanel.resetpwd.mis-chance.period');
-		if(!$count = $cache->get("packages.userpanel.resetpwd.bruteforce.".http::$client['ip'])){
-			$count = 1;
-		}
-		$cache->set("packages.userpanel.resetpwd.bruteforce.".http::$client['ip'], $count + 1, $period);
-	}
-	public function reset(){
-		$view = view::byName("\\packages\\userpanel\\views\\resetpwd");
-		$inputsRules = [
-			'username' => [
-				'type' => ['email', 'cellphone']
-			],
-			'method' => [
+	public function reset() {
+		$view = View::byName(views\resetpwd::class);
+		$view->setData($this->getChannelsNames(), "channelsnames");
+		$inputsRules = array(
+			'username' => array(
+				'type' => array('email', 'cellphone'),
+			),
+			'method' => array(
 				'type' => 'string',
-				'values' => ['sms', 'email']
-			]
-		];
-		try{
+				'values' => $this->getChannelsNames(),
+			),
+		);
+		try {
 			$this->haveChance();
 			$this->response->setStatus(true);
 			$inputs = $this->checkinputs($inputsRules);
-			switch($inputs['method']){
-				case('sms'):
-					$user = new user();
-					$user->where('cellphone', $inputs['username']);
-					if(!$user = $user->getOne()){
-						$this->loseOneChance();
-						throw new inputValidation('username');
-					}
-					$this->sendSMS($user);
-					break;
-				case('email'):
-					$user = new user();
-					$user->where('email', $inputs['username']);
-					if(!$user = $user->getOne()){
-						$this->loseOneChance();
-						throw new inputValidation('username');
-					}
-					$this->sendEmail($user);
-					break;
+			if ($inputs["method"] == "sms") {
+				$prob = "cellphone";
+			} elseif ($inputs['method'] == "email") {
+				$prob = "email";
+			} else {
+				// What should I do with this?
+				throw new InputValidation('method');
 			}
+			$user = (new User)->where($prob, $inputs['username'])->getOne();
+			if(!$user){
+				$this->loseOneChance();
+				throw new InputValidation('username');
+			}
+			$this->notifyUser($user, $inputs["method"]);
 			$this->response->setData($inputs['username'], 'username');
-		}catch(inputValidation $error){
+		} catch (InputValidation $error) {
 			$view->setFormError(FormError::fromException($error));
 			$this->response->setStatus(false);
-		}catch(losingChance $error){
+		} catch (losingChance $error) {
 			$error = $this->losingChanceError();
 			$view->addError($error);
 			$this->response->setStatus(false);
@@ -126,32 +84,8 @@ class resetpwd  extends controller{
 		$this->response->setView($view);
 		return $this->response;
 	}
-	private function sendSMS(user $user){
-		$token = new token();
-		$token->token = rand(10000, 99999);
-		$token->user = $user->id;
-		$token->ip = http::$client['ip'];
-		$api = new smsAPI();
-		$api->template('userpanel_resetpwd_token', ['token' => $token]);
-		$api->to($user->cellphone, $user);
-		$api->send();
-		$token->sent_at = date::time();
-		$token->save();
-	}
-	private function sendEmail(user $user){
-		$token = new token();
-		$token->token = md5(rand(1000, 999999));
-		$token->user = $user->id;
-		$token->ip = http::$client['ip'];
-		$api = new emailAPI();
-		$api->template('userpanel_resetpwd_token', ['token' => $token]);
-		$api->to($user->email, $user);
-		$api->send();
-		$token->sent_at = date::time();
-		$token->save();
-	}
-	public function authenticationToken(){
-		$view = view::byName("\\packages\\userpanel\\views\\resetpwd");
+	public function authenticationToken() {
+		$view = view::byName(views\resetpwd::class);
 		$inputsRules = [
 			'token' => [
 				'type' => 'number'
@@ -160,30 +94,30 @@ class resetpwd  extends controller{
 				'type' => 'cellphone'
 			]
 		];
-		try{
+		try {
 			$this->haveChance();
 			$this->response->setStatus(false);
 			$inputs = $this->checkinputs($inputsRules);
-			$user = new user();
+			$user = new User();
 			$user->where('cellphone', $inputs['username']);
 			if(!$user = $user->getOne()){
 				throw new NotFound();
 			}
-			$token = new token();
+			$token = new Token();
 			$token->where('user', $user->id);
-			$token->where('sent_at', date::time() - 7200, '>');
+			$token->where('sent_at', Date::time() - 7200, '>');
 			$token->where('token', $inputs['token']);
 			$token->orderBy('sent_at', 'DESC');
 			if(!$token = $token->getOne()){
-				throw new inputValidation('token');
+				throw new InputValidation('token');
 			}
 			$token->delete();
-			login::doLogin($user);
+			Login::doLogin($user);
 			$this->response->setStatus(true);
 			$this->response->Go(userpanel\url('resetpwd/newpwd'));
-		}catch(inputValidation $error){
+		} catch (InputValidation $error) {
 			$view->setFormError(FormError::fromException($error));
-		}catch(losingChance $error){
+		} catch (losingChance $error) {
 			$error = $this->losingChanceError();
 			$view->addError($error);
 			$this->response->setStatus(false);
@@ -191,23 +125,78 @@ class resetpwd  extends controller{
 		$this->response->setView($view);
 		return $this->response;
 	}
-	public function authenticationEmailToken($data){
-		try{
+	public function authenticationEmailToken($data) {
+		try {
 			$this->loseOneChance();
-		}catch(losingChance $error){
+		} catch (losingChance $error) {
 			throw new NotFound();
 		}
-		$token = new token();
+		$token = new Token();
 		$token->where('token', $data['token']);
-		$token->where('sent_at', date::time() - 86400, '>');
-		if(!$token = $token->getOne()){
+		$token->where('sent_at', Date::time() - 86400, '>');
+		if (!$token = $token->getOne()) {
 			throw new NotFound();
 		}
-		login::doLogin($token->user);
+		Login::doLogin($token->user);
 		$this->response->setStatus(true);
 		$token->delete();
 		$this->response->Go(userpanel\url('resetpwd/newpwd'));
 		return $this->response;
 	}
+	private function getChannels() {
+		$channels = new Channels();
+		$channels->trigger();
+		return $channels->get();
+	}
+	private function getChannelByName(string $name) {
+		foreach ($this->getChannels() as $channel) {
+			if ($channel->getName() == $name) {
+				return $channel;
+			}
+		}
+		return null;
+	}
+	private function getChannelsNames(): array {
+		$names = array();
+		foreach ($this->getChannels() as $channel) {
+			$names[] = $channel->getName();
+		}
+		return $names;
+	}
+	private function haveChance() {
+		$cache = new Cache();
+		$times = Options::get('userpanel.resetpwd.mis-chance.count');
+		if ($cache->get("packages.userpanel.resetpwd.bruteforce." . http::$client['ip']) > $times) {
+			throw new losingChance();
+		}
+	}
+	private function losingChanceError() {
+		$period = Options::get('userpanel.resetpwd.mis-chance.period');
+		$times = Options::get('userpanel.resetpwd.mis-chance.count');
+		$error = new Error();
+		$error->setCode('userpanel.resetpwd.losingChance');
+		$error->setMessage(t('error.userpanel.resetpwd.losingChance', [
+			'times' => $times,
+			'expire_at' => Date::relativeTime(Date::time() + $period)
+		]));
+		return $error;
+	}
+	private function loseOneChance() {
+		$cache = new Cache();
+		$period = Options::get('userpanel.resetpwd.mis-chance.period');
+		if(!$count = $cache->get("packages.userpanel.resetpwd.bruteforce.".http::$client['ip'])){
+			$count = 1;
+		}
+		$cache->set("packages.userpanel.resetpwd.bruteforce." . http::$client['ip'], $count + 1, $period);
+	}
+	private function notifyUser(User $user, string $method) {
+		$token = new Token();
+		$token->token = ($method == "sms") ? rand(10000, 99999) : md5(rand(1000, 999999));
+		$token->user = $user->id;
+		$token->ip = http::$client['ip'];
+		$this->getChannelByName($method)->notify(events\ResetPWD($token));
+		$token->sent_at = Date::time();
+		$token->save();
+	}
 }
-class losingChance extends \Exception{}
+class losingChance extends Exception {}
