483bc5cc4d921557a2b60f3fb28f0dc02bf1d6bb
Mahdi Abedi <abedi@jeyserver.com>
2019-12-28 18:16:47 +0330

    get user permissions in view profile

--- views/profile/view.php
+++ views/profile/view.php
@@ -33,6 +33,7 @@ class view extends \packages\userpanel\view{
 					"status" => $user->status,
 					"avatar" => $user->avatar,
 				),
+				"permissions" => $user->getPermissions(),
 			),
 		);
 		if (packages::package("financial")) {
@@ -40,6 +41,7 @@ class view extends \packages\userpanel\view{
 		} else {
 			$data["data"]["user"]["currency"] = options::get("packages.userpanel.users.credit.currency.title");
 		}
+
 		return $data;
 	}
 }
