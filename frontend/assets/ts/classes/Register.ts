afe09eb54737b25eaa93d0e92cc792cbc4e7d323
Mahdi Abedi <abedi@jeyserver.com>
2020-02-03 15:05:47 +0330

    Fix #58

--- frontend/assets/ts/classes/Register.ts
+++ frontend/assets/ts/classes/Register.ts
@@ -9,11 +9,11 @@ import "./jquery.formAjax";
 import { webuilder } from "webuilder";
 
 export class Register{
-	private static form = $('.form-register');
-	private static errorHandler = $('.errorHandler', Register.form);
+	private static $form = $('.form-register');
+	private static $errorHandler = $('.errorHandler', Register.$form);
 	private static runRegisterValidator():void {
 		Main.importValidationTranslator();
-        Register.form.validate({
+        Register.$form.validate({
             rules: {
                 name: {
                     required: true
@@ -59,20 +59,23 @@ export class Register{
 				}
         	},
             submitHandler: (form) => {
+				Register.$errorHandler.removeClass("alert-success").addClass("alert-danger").hide();
 				$(form).formAjax({
 					success: function(data: webuilder.AjaxResponse){
 						window.location.href = data.redirect;
 					},
-					error: function(error:webuilder.AjaxError){
-						if(error.error == 'data_duplicate' || error.error == 'data_validation'){
-							let $input = $('[name='+error.input+']');
+					error: function(response:webuilder.AjaxError){
+						if (response.error === "user_status_is_deactive_in_register" || response.error === "user_status_is_suspend_in_register") {
+							Register.$errorHandler.removeClass("alert-danger").addClass("alert-success").html(t(`error.${response.error}`)).show();
+						} else if(response.error == 'data_duplicate' || response.error == 'data_validation'){
+							let $input = $('[name='+response.input+']');
 							let $params = {
 								title: t("error.fatal.title"),
 								message:''
 							};
-							if (error.error == 'data_duplicate') {
-								$params.message = t(`user.${error.input}.data_duplicate`);
-							} else if (error.error == 'data_validation') {
+							if (response.error == 'data_duplicate') {
+								$params.message = t(`user.${response.input}.data_duplicate`);
+							} else if (response.error == 'data_validation') {
 								$params.message = t("data_validation");
 							}
 							if($input.length){
@@ -81,13 +84,13 @@ export class Register{
 								$.growl.error($params);
 							}
 						}else{
-							Register.errorHandler.html(`<i class="fa fa-remove-sign"></i> ${t("userpanel.formajax.error")}`).show();
+							Register.$errorHandler.html(`<i class="fa fa-times-circle"></i> ${t("userpanel.formajax.error")}`).show();
 						}
 					}
 				});
 			},
             invalidHandler: function (event, validator) {
-                Register.errorHandler.html(Register.errorHandler.data('orghtml')).show();
+                Register.$errorHandler.html(Register.$errorHandler.data('orghtml')).show();
             }
 		});
 		
