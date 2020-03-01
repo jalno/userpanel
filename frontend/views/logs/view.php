e4ad107a6607df9dc01ff1d4e312c4129aadf533
Ali Abedi <aliabedi@jeyserver.com>
2020-03-01 17:41:07 +0330

    Fix #68

--- frontend/views/logs/view.php
+++ frontend/views/logs/view.php
@@ -1,6 +1,6 @@
 <?php
 namespace themes\clipone\views\logs;
-
+use packages\userpanel;
 use packages\userpanel\views;
 use themes\clipone\{Navigation, Breadcrumb, ViewTrait};
 
@@ -19,12 +19,24 @@ class View extends Views\Logs\View{
 		$this->setNavigation();
 	}
 	private function setNavigation(){
-
-		Breadcrumb::addItem(Navigation::getByName("logs"));
-		$item = new Navigation\MenuItem("log");
-		$item->setTitle($this->getTitle());
-		$item->setIcon('fa fa-exclamation-circle');
-		Breadcrumb::addItem($item);
-		Navigation::active("logs");
+		if (Navigation::getByName("logs") != null) {
+			Breadcrumb::addItem(Navigation::getByName("logs"));
+			$item = new Navigation\MenuItem("log");
+			$item->setTitle($this->getTitle());
+			$item->setIcon('fa fa-exclamation-circle');
+			Breadcrumb::addItem($item);
+			Navigation::active("logs");
+		}else{
+			Navigation::active("dashboard");
+			$item = new Navigation\MenuItem("profile");
+			$item->setTitle(t('profile.view'));
+			$item->setURL(userpanel\url('profile/view'));
+			$item->setIcon('clip-user');
+			Breadcrumb::addItem($item);
+			$item = new Navigation\MenuItem("profile-view");
+			$item->setTitle($this->getTitle());
+			$item->setIcon('fa fa-exclamation-circle');
+			Breadcrumb::addItem($item);
+		}
 	}
 }
