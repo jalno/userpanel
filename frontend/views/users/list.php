04a0ef0e68b7312323419a78a25c4b2407d55bad
Ali Abedi <aliabedi@jeyserver.com>
2020-04-02 12:27:07 +0430

    Fix #77

--- frontend/views/users/list.php
+++ frontend/views/users/list.php
@@ -59,10 +59,6 @@ class listview extends usersListView{
 	public function getStatusForSelect(){
 		return array(
 			array(
-				'title' => '',
-				'value' => ''
-			),
-			array(
 				'title' => t('user.status.active'),
 				'value' => user::active
 			),
@@ -73,6 +69,10 @@ class listview extends usersListView{
 			array(
 				'title' => t('user.status.deactive'),
 				'value' => user::deactive
+			),
+			array(
+				'title' => '',
+				'value' => ''
 			)
 		);
 	}
