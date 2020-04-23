1297cfa835d1cbbe799772bbbe1e39906dd790b7
Mahdi Abedi <abedi@jeyserver.com>
2020-04-23 19:39:25 +0430

    Handle show error in register page

--- frontend/assets/ts/classes/Register.ts
+++ frontend/assets/ts/classes/Register.ts
@@ -76,21 +76,38 @@ export class Register {
 						window.location.href = data.redirect;
 					},
 					error: (response) => {
-						if (response.error === "user_status_is_deactive_in_register" || response.error === "user_status_is_suspend_in_register") {
-							Register.$errorHandler.removeClass("alert-danger").addClass("alert-success").html(t(`error.${response.error}`)).show();
-						} else if (response.error === "data_duplicate" || response.error === "data_validation") {
-							const $input = $(`[name="${response.input}"]`);
-							const params = {
-								title: t("error.fatal.title"),
-								message: "",
-							};
+						if (response.hasOwnProperty("error") || response.hasOwnProperty("code")) {
 							const code = response.hasOwnProperty("error") ? response.error : response.code;
-							if (code === "data_duplicate") {
-								params.message = t(`user.${response.input}.data_duplicate`);
-							} else if (code === "data_validation") {
-								params.message = t("data_validation");
+							if (code === "data_duplicate" || code === "data_validation") {
+								const $input = $(`[name="${response.input}"]`);
+								const params = {
+									title: t("error.fatal.title"),
+									message: "",
+								};
+								const code = response.hasOwnProperty("error") ? response.error : response.code;
+								if (code === "data_duplicate") {
+									params.message = t(`user.${response.input}.data_duplicate`);
+								} else if (code === "data_validation") {
+									params.message = t("data_validation");
+								} else {
+									let message;
+									if (response.hasOwnProperty("message") && response.message) {
+										message = response.message;
+									} else {
+										message = t(`error.${code}`);
+									}
+									if (!message) {
+										message = t("userpanel.formajax.error");
+									}
+									Register.$errorHandler.html(`<i class="fa fa-times-circle"></i> ${message}`).show();
+								}
+								if ($input.length) {
+									$input.inputMsg(params);
+								} else {
+									$.growl.error(params);
+								}
 							} else {
-								let message;
+								let message = "";
 								if (response.hasOwnProperty("message") && response.message) {
 									message = response.message;
 								} else {
@@ -99,12 +116,7 @@ export class Register {
 								if (!message) {
 									message = t("userpanel.formajax.error");
 								}
-								Register.$errorHandler.html(`<i class="fa fa-times-circle"></i> ${message}`).show();
-							}
-							if ($input.length) {
-								$input.inputMsg(params);
-							} else {
-								$.growl.error(params);
+								Register.$errorHandler.removeClass("alert-danger").addClass("alert-success").html(message).show();
 							}
 						} else {
 							Register.$errorHandler.html(`<i class="fa fa-times-circle"></i> ${t("userpanel.formajax.error")}`).show();
