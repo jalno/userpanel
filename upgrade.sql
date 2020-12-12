85b6ae879f14d2986e0282ad133c4859667d774d
Hossein Hosni <hosni.hossein@gmail.com>
2020-12-12 12:58:43 +0330

    #111 Update upgrade.sql

--- upgrade.sql
+++ upgrade.sql
@@ -18,3 +18,8 @@ UPDATE `userpanel_users` SET `status` = 3 WHERE `status` = 0;
 --	Commit: 6720e0f95aef2a65b2b98e7db0498d5d8c495b21
 --
 ALTER TABLE `userpanel_logs` CHANGE `user` `user` INT(11) NULL;
+
+---
+---	Commit: 82fb3bc6e09ecbe8611e60c388331bada1d962c2
+---
+ALTER TABLE `userpanel_logs` CHANGE `parameters` `parameters` LONGTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
