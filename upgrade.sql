592761a085b7d172e343fd58c71715d7d9d5f1d1
Hossein Hosni <hosni.hossein@gmail.com>
2020-12-31 18:29:03 +0330

    #114 Update upgrade.sql

--- upgrade.sql
+++ upgrade.sql
@@ -23,3 +23,14 @@ ALTER TABLE `userpanel_logs` CHANGE `user` `user` INT(11) NULL;
 ---	Commit: 82fb3bc6e09ecbe8611e60c388331bada1d962c2
 ---
 ALTER TABLE `userpanel_logs` CHANGE `parameters` `parameters` LONGTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL;
+
+--
+--	Commit: 6b5c7dd039f240ba0e910bbee1cb0760618001fe
+--
+ALTER TABLE `userpanel_users` ADD `custom_permissions` TINYINT UNSIGNED NOT NULL DEFAULT '0' AFTER `registered_at`;
+CREATE TABLE `userpanel_users_permissions` (
+	`user_id` int(11) NOT NULL,
+	`permission` varchar(255) NOT NULL,
+	PRIMARY KEY (`user_id`,`permission`),
+	CONSTRAINT `userpanel_users_permissions_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `userpanel_users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
